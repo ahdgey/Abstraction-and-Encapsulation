a16b824199c2b82c951f6b0d04d2416cdfdd130f
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-04 01:37:14 +0800

    Speed, On, Radius, and Color

--- Fan.py
+++ Fan.py
@@ -18,11 +18,20 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
-    
-#A private int data field named on that specifies the speed of the fan.
-#A private bool data field named on that specifies whether the fan is on (the default is False).
-#A private float field named radius that specifies the radius of the fan.
-#A private string data field named color that specifies the color of the fan
+
+    def __init__(self, speed=SLOW, radius=5, color='blue', on=False):
+        #A private int data field named on that specifies the speed of the fan.
+        self.__speed = speed
+
+        #A private bool data field named on that specifies whether the fan is on (the default is False).
+        self.__on = on
+
+        #A private float field named radius that specifies the radius of the fan.
+        self.__radius = radius
+
+        #A private string data field named color that specifies the color of the fan.
+        self.__color = color
+        
 #The accessor(getters) methods for all four data fields.
 #The mutator(setters) methods for all four data fields.
       
