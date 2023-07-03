a7e9d27a9942800c1b74c45b0a84a0d58b710fee
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-04 01:19:28 +0800

    Programmers name, course, and section

--- Fan.py
+++ Fan.py
@@ -7,4 +7,7 @@ print("033[0;36m*" * 90)
 print("033[1;97mThe Fan Class".center(97))
 print("\033[0;36m*" * 90)
 
+print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
+print("\033[1;32m\nShe's from BSCPE 1-4")
+print("-" * 90)
       
