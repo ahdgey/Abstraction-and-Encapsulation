517487ddbec505e719d6002a4abd31eb3b7bbc20
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-04 01:56:20 +0800

    Pseudocode

--- TestFan.py
+++ TestFan.py
@@ -6,4 +6,6 @@ class TestFan:
         fan_1 = Fan(Fan.FAST, 10, 'yellow', True)
         fan_2 = Fan(Fan.MEDIUM, 5, 'blue', False)
 
-        
+    #Fan number 1, assign maximum speed, radius 10, color yellow, and turm it on.
+    #Fan number 2, assign medium speed, radius 5, color blue, and turn it off.
+    
