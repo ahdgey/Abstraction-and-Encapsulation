369152a043de6e5f82c37196800363742ec56b55
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-04 02:26:52 +0800

    Add some color

--- TestFan.py
+++ TestFan.py
@@ -7,17 +7,17 @@ class TestFan:
         fan_2 = Fan(Fan.MEDIUM, 5, 'blue', False)
 
         #Fan number 1, assign maximum speed, radius 10, color yellow, and turm it on.
-        print("Fan 1:")
-        print("The speed of the fan:", fan_1.get_speed())
-        print("The radius of the fan:", fan_1.get_radius())
-        print("The color of the fan:", fan_1.get_color())
-        print("The fan is on:", fan_1.is_on())
+        print("\033[0;33mFan 1:")
+        print("\033[0;31mThe speed of the fan: \033[1;37m", fan_1.get_speed())
+        print("\033[0;31mThe radius of the fan: \033[1;37m", fan_1.get_radius())
+        print("\033[0;31mThe color of the fan: \033[1;37m", fan_1.get_color())
+        print("\033[0;31mThe fan is on: \033[1;37m", fan_1.is_on())
 
         #Fan number 2, assign medium speed, radius 5, color blue, and turn it off.
-        print("\nFan 2:")
-        print("The speed of the fan:", fan_2.get_speed())
-        print("The radius of the fan:", fan_2.get_radius())
-        print("The color of the fan:", fan_2.get_color())
-        print("The fan is on:", fan_2.is_on())
+        print("\033[0;33m\nFan 2:")
+        print("\033[0;31mThe speed of the fan: \033[1;37m", fan_2.get_speed())
+        print("\033[0;31mThe radius of the fan: \033[1;37m", fan_2.get_radius())
+        print("\033[0;31mThe color of the fan: \033[1;37m", fan_2.get_color())
+        print("\033[0;31mThe fan is on: \033[1;37m", fan_2.is_on())
 
 TestFan()
