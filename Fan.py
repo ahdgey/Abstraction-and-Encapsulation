a3c9005109bfa097de1ed712817dacc769f5660c
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-04 01:30:02 +0800

    Pseudocode

--- Fan.py
+++ Fan.py
@@ -10,4 +10,13 @@ print("\033[0;36m*" * 90)
 print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
 print("\033[1;32m\nShe's from BSCPE 1-4")
 print("-" * 90)
+
+#(The Fan Class) Design a class named Fan to represent a fan. The class contains:
+#Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, 3 to denote the fan speed.
+#A private int data field named on that specifies the speed of the fan.
+#A private bool data field named on that specifies whether the fan is on (the default is False).
+#A private float field named radius that specifies the radius of the fan.
+#A private string data field named color that specifies the color of the fan
+#The accessor(getters) methods for all four data fields.
+#The mutator(setters) methods for all four data fields.
       
