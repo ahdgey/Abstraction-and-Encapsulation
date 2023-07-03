e9a756f5beadbbdb853bf53706ae7193afd2ef1f
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-04 02:18:26 +0800

    Fan 2

--- TestFan.py
+++ TestFan.py
@@ -14,3 +14,10 @@ class TestFan:
         print("The fan is on:", fan_1.is_on())
 
         #Fan number 2, assign medium speed, radius 5, color blue, and turn it off.
+        print("\nFan 2:")
+        print("The speed of the fan:", fan_2.get_speed())
+        print("The radius of the fan:", fan_2.get_radius())
+        print("The color of the fan:", fan_2.get_color())
+        print("The fan is on:", fan_2.is_on())
+
+TestFan()
