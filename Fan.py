ad66f2c66c439d895ee54b65d498b8edc7e474f6
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-04 01:16:54 +0800

    Title

--- Fan.py
+++ Fan.py
@@ -1,3 +1,10 @@
 #Alexza Jean R. Catanoy
 #BSCPE 1-4
 #Abstraction and Encasulation
+
+#Title
+print("033[0;36m*" * 90)
+print("033[1;97mThe Fan Class".center(97))
+print("\033[0;36m*" * 90)
+
+      
