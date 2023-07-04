c6ca39a2a213b72af4bec5e3e16fc73214d64aed
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-05 02:41:03 +0800

    Pseudocode

--- TestPet.py
+++ TestPet.py
@@ -3,4 +3,10 @@ from Pet import Pet
 class TestPet:
 
     def Test(self):
+
+        #Write a program that create an object of the class.
+        #Prompts the user to enter the name of pet.
+        #Prompts the user to enter the type of pet.
+        #Prompts the user to enter the age of the pet
+        #Display the output.
         
