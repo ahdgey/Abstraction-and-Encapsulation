8937df078aae5b02c480e20e794c5002673accf2
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-05 02:22:44 +0800

    Pseudocode

--- Pet.py
+++ Pet.py
@@ -10,3 +10,22 @@ print("\033[0;36m*" * 90)
 print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
 print("\033[1;32m\nShe's from BSCPE 1-4")
 print("\033[1;32m-" * 90)
+
+#Write a class named Pet, which should have the following data attributes:
+#The Pet class should have an _ _ init_ _ method that creates these attributes.
+#_ _name (for the names of pet)
+#_ _animal_type (for the type of animal that a pet is. Example values are 'Dog', 'Cat', and 'Bird')
+#_ _age (for the pet's age)
+#It should also have the following methods:
+#set_name()
+#This method assigns a value to the _ _name field.
+#set_animal_type()
+#This method aasigns a value to the _ _animal_type field.
+#set_age()
+#This method assigns a value to the _ _age field.
+#get_name()
+#This method returns the value of the _ _name field.
+#get_animal_type()
+#This method returns the value of the _ _animal_type field.
+#get_age()
+#This method returns the value of the _ _age field.
