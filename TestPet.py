47188103ea10b0fa66b12e89bf5c9b433e46cc46
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-05 02:42:47 +0800

    Pet Name

--- TestPet.py
+++ TestPet.py
@@ -5,8 +5,12 @@ class TestPet:
     def Test(self):
 
         #Write a program that create an object of the class.
+        pet = Pet()
+
         #Prompts the user to enter the name of pet.
+        name = str(inout("Kindly type in the name of your pet: "))
+        pet.set_name(name)
+        
         #Prompts the user to enter the type of pet.
         #Prompts the user to enter the age of the pet
         #Display the output.
-        
