2ec3ca9c541c8c0824f85f077f09cb7b00ca2f37
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-05 01:23:36 +0800

    Pseudocode

--- TestCar.py
+++ TestCar.py
@@ -5,3 +5,6 @@ class TestCar:
     def Test(self):
         car = Car(2023, "Mazda 5")
         car.show()
+
+        #Call the accelerate method five times.
+        #Call the brake method five times. 
