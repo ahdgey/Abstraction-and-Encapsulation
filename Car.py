cefbf033537e40119d22928622a6c90b92062b4e
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-05 00:50:12 +0800

    Pseudocode

--- Car.py
+++ Car.py
@@ -10,3 +10,15 @@ print("\033[0;36m*" * 90)
 print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
 print("\033[1;32m\nShe's from BSCPE 1-4")
 print("-" * 90)
+
+#Write a class named Car that has the following data attributes:
+#_ _year_model(for the car's year model)
+#_ _make(for the make of the car)
+#_ _speed(fpr the car's current speed)
+#The class should also have the following methods:
+#accelerate()
+#The accelerate method should add 5 to the speed data attribute each time it is called.
+#brake()
+#The brake method should subtract 5 from the speed data attribute each time it is called.
+#get_speed()
+#The get_speed should return the current speed.
