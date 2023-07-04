25b37252cc70ffd96d4fc2c50b0ff42007c3bc07
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-07-05 02:12:12 +0800

    Programmers name, course, year level, and section

--- Pet.py
+++ Pet.py
@@ -6,3 +6,7 @@
 print("\033[0;36m*" * 90)
 print("\033[1;97mThe Pet Class".center(97))
 print("\033[0;36m*" * 90)
+
+print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
+print("\033[1;32m\nShe's from BSCPE 1-4")
+print("\033[1;32m-" * 90)
